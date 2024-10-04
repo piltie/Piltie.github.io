c1dd97348b0a424e96f8d5616883de52740acbee
Sarah Carla Souza <sarah.souza@foa.org.br>
2024-10-04 14:17:40 -0300

    refactor: removing boilerplate by adding the initial code structure

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import PageHeader from "../components/PageHeader.vue";
+import PageMenu from "../components/PageMenu.vue";
+import SectionAbout from "../components/SectionAbout.vue";
+import SectionExperience from "../components/SectionExperience.vue";
+import SectionSkills from "../components/SectionSkills.vue";
+import SectionContact from "../components/SectionContact.vue";
+
+const about = "about section";
+const experience = "experience section";
+const skills = "skills section";
+const contact = "contact section";
+</script>
+
+<template>
+  <PageHeader />
+  <PageMenu />
+  <SectionAbout :text="about" />
+  <SectionExperience :text="experience" />
+  <SectionSkills :text="skills" />
+  <SectionContact :text="contact" />
+</template>
