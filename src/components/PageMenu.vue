13ef55edf8851dbc438b26e2971e168ab38d038a
Sarah Carla Souza <sarah.souza@foa.org.br>
2024-10-11 18:54:44 -0300

    feature: add menu skeleton

--- src/components/PageMenu.vue
+++ src/components/PageMenu.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>menu</div>
+  <ul class="text-subtitle-medium tracking-widest">
+    <li>SOBRE</li>
+    <li>EXPERIÊNCIA</li>
+    <li>SKILLS</li>
+    <li>CONTATO</li>
+  </ul>
 </template>
