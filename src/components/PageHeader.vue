2de2eb1e382ba1f69dce6649dbb2c3166991e9ab
Sarah Carla Souza <sarah.souza@foa.org.br>
2024-10-11 18:55:08 -0300

    style: changes header line spacing and size

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-brown text-pale py-[25.5px] text-center font-semibold font-dmsans tracking-[25%]"
+    class="bg-brown text-pale py-[25.5px] text-center text-subtitle-medium font-semibold font-dmsans tracking-widest"
   >
     EN | PT
   </div>
