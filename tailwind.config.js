da20847a8ec323a9e82e8019809d5ff6e0c62a58
Sarah <95034183+piltie@users.noreply.github.com>
2024-10-11 01:15:19 -0300

    style: adds color palette and font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      pale: "#EFE9E4",
+      light: "#AB8F6E",
+      brown: "#5E5343",
+    },
+    fontSize: {
+      "title-large": "96px",
+      "title-medium": "40px",
+      "title-small": "30px",
+      "subtitle-large": "24px",
+      "subtitle-medium": "20px",
+      "subtitle-small": "14px",
+      "paragraph-large": "20px",
+      "paragraph-medium": "16px",
+    },
   },
   plugins: [],
 };
